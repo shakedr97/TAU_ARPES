e73c9f8baad4ebc25bdb0a5db976e0bbe35313e4
shakedr97 <shakedrosesntein@gmail.com>
2023-01-29 15:19:01 +0200

    add igor text export option to both integrated and raw spectrum

--- tau_daq.py
+++ tau_daq.py
@@ -6,7 +6,7 @@ import enum
 import PEAK.DA30 as DA30
 from PyQt5.QtCore import QSize, QRunnable, QThreadPool, pyqtSlot
 from PyQt5.QtCore import Qt as Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QSlider
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QSlider, QComboBox
 from PyQt5.QtGui import QPixmap
 import matplotlib as plt
 
@@ -237,7 +237,7 @@ class SweepData:
         view.set_ylabel(y_axis["Label"] + " [" + y_axis["Unit"] + "]")
 
     def export(self, file_name, column_delimiter='\t', row_delimiter='\n'):
-        with open(f'exports\\{file_name}', 'w') as f:
+        with open(f'exports\\{file_name}.txt', 'w') as f:
             f.write(f'time{column_delimiter}counts{row_delimiter}')
             for point in self.sweep:
                 f.write(
@@ -245,7 +245,7 @@ class SweepData:
                 )
 
     def export_raw(self, file_name, column_delimiter='\t', row_delimiter='\n'):
-        with open(f'exports\\{file_name}', 'w') as f:
+        with open(f'exports\\{file_name}.txt', 'w') as f:
             for point in self.sweep:
                 for row in self.sweep[point]:
                     line = column_delimiter
@@ -259,16 +259,44 @@ class SweepData:
                          file_name,
                          column_delimiter='\t',
                          row_delimiter='\n'):
-        with open(f'exports\\{file_name}', 'w') as f:
+        with open(f'exports\\{file_name}.itx', 'w') as f:
             f.write(f'IGOR{row_delimiter}')
-            f.write(f'WAVES/D/N=({len(self.sweep)}, 2) wave0{row_delimiter}'
+            f.write(f'WAVES/D/N=({len(self.sweep)}, 2) counts, time{row_delimiter}'
                     )  # FIXME: proper wave dimensions for arbitrary data
             f.write(f'BEGIN{row_delimiter}')
             for point in self.sweep:
                 f.write(
-                    f'{column_delimiter}{point}{column_delimiter}{self.sweep[point]}{row_delimiter}'
+                    f'{column_delimiter}{point}{column_delimiter}{sum(sum(self.sweep[point]))}{row_delimiter}'
                 )
             f.write('END')
+
+    def export_raw_igor_text(self,
+                             file_name,
+                             column_delimiter='\t',
+                             row_delimiter='\n'):
+        with open(f'exports\\{file_name}.itx', 'w') as f:
+            f.write(f'IGOR{row_delimiter}')
+            index = 0
+            for point in self.sweep:
+                index += 1
+                shape = self.sweep[point].shape
+                f.write(f'WAVES/D/N=({shape[0]}, {shape[1]}) delay_{index}{row_delimiter}'
+                        )  # FIXME: proper wave dimensions for arbitrary data
+                f.write(f'BEGIN{row_delimiter}')
+                for row in self.sweep[point]:
+                    line = column_delimiter
+                    for element in row:
+                        line += f'{element:.4}{column_delimiter}'
+                    line += row_delimiter
+                    f.write(line)
+                f.write('END')
+                f.write(row_delimiter)
+            f.write(f'WAVES/D sweep_points{row_delimiter}')
+            f.write(f'BEGIN{row_delimiter}')
+            for point in self.sweep:
+                f.write(f'{column_delimiter}{point}{row_delimiter}')
+            f.write('END')
+            f.write(row_delimiter)
 
 
 class ExportWorker(QRunnable):
@@ -284,13 +312,21 @@ class ExportWorker(QRunnable):
         try:
             if self.is_export_raw:
                 self.gui.export_spectrum_indicator.set_ongoing()
-                self.export_data.export_raw(
-                    self.gui.export_spectrum_name_input.text())
+                if self.gui.export_spectrum_format.currentText() == 'txt':
+                    self.export_data.export_raw(
+                        self.gui.export_spectrum_name_input.text())
+                elif self.gui.export_spectrum_format.currentText() == 'itx':
+                    self.export_data.export_raw_igor_text(
+                        self.gui.export_spectrum_name_input.text())
                 self.gui.export_spectrum_indicator.set_blank()
             else:
                 self.gui.export_sweep_indicator.set_ongoing()
-                self.export_data.export(
-                    self.gui.export_sweep_name_input.text())
+                if self.gui.export_sweep_format.currentText() == 'txt':
+                    self.export_data.export(
+                        self.gui.export_sweep_name_input.text())
+                elif self.gui.export_sweep_format.currentText() == 'itx':
+                    self.export_data.export_igor_text(
+                        self.gui.export_sweep_name_input.text())
                 self.gui.export_sweep_indicator.set_blank()
         except Exception as e:
             if self.is_export_raw:
@@ -497,7 +533,9 @@ class DaqWindow(QMainWindow):
         self.spectrum = SpectrumCanvas()
         self.export_spectrum_controls = QHBoxLayout()
         self.export_spectrum_name_label = QLabel('file name')
-        self.export_spectrum_name_input = QLineEdit('spectrum_data.txt')
+        self.export_spectrum_name_input = QLineEdit('spectrum_data')
+        self.export_spectrum_format = QComboBox()
+        self.export_spectrum_format.addItems(['txt', 'itx'])
         self.export_spectrum_button = QPushButton('export spectrum data')
         self.export_spectrum_button.clicked.connect(self.do_export_spectrum)
         self.export_spectrum_indicator = Indicator()
@@ -505,6 +543,7 @@ class DaqWindow(QMainWindow):
             self.export_spectrum_name_label)
         self.export_spectrum_controls.addWidget(
             self.export_spectrum_name_input)
+        self.export_spectrum_controls.addWidget(self.export_spectrum_format)
         self.export_spectrum_controls.addWidget(self.export_spectrum_button)
         self.export_spectrum_controls.addWidget(self.export_spectrum_indicator)
         self.spectrum_layout.addWidget(self.spectrum)
@@ -515,12 +554,15 @@ class DaqWindow(QMainWindow):
         self.sweep_canvas = SweepCanvas()
         self.export_sweep_controls = QHBoxLayout()
         self.export_sweep_name_label = QLabel('file name')
-        self.export_sweep_name_input = QLineEdit('sweep_data.txt')
+        self.export_sweep_name_input = QLineEdit('sweep_data')
+        self.export_sweep_format = QComboBox()
+        self.export_sweep_format.addItems(['txt', 'itx'])
         self.export_sweep_button = QPushButton('export sweep data')
         self.export_sweep_button.clicked.connect(self.do_export_sweep)
         self.export_sweep_indicator = Indicator()
         self.export_sweep_controls.addWidget(self.export_sweep_name_label)
         self.export_sweep_controls.addWidget(self.export_sweep_name_input)
+        self.export_sweep_controls.addWidget(self.export_sweep_format)
         self.export_sweep_controls.addWidget(self.export_sweep_button)
         self.export_sweep_controls.addWidget(self.export_sweep_indicator)
         self.sweep_layout.addWidget(self.sweep_canvas)
